c8210db6ccfa145fbd1a48b1de771c0a2295b22f
boriss02 <borismateoic@gmail.com>
2023-07-02 20:04:54 -0300

    boriss

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 function laCajaDePandora(numero) {
-    if (numero % 2 === 0) {
-        return numero.toString(2); // Convierte a binario
-    } else {
-        return numero.toString(16); // Convierte a hexadecimal
-    }
-}
-
-
-// hola god
+    // Rama de Boris
+};
